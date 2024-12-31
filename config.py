1e5871a714d6cedce55d5450ad2e252808aba78d
Spider-Man <theirlwolf@gmail.com>
2024-12-31 23:24:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,7 +67,7 @@ VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://youtube.com/shorts/TpU
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://irlwolfhub.blogspot.com/2024/12/welcome-to-irlwolfhub.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
